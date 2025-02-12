4afc29994fc44097f9f3f334a4b5305a50c849b6
Zilja717 <96008554+Zilja717@users.noreply.github.com>
2025-02-12 14:23:26 +0700

    Update nu_vot_eto_tochno_krutaya_igruha.py

--- nu_vot_eto_tochno_krutaya_igruha.py
+++ nu_vot_eto_tochno_krutaya_igruha.py
@@ -5,7 +5,7 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 400
 SPEED = 10
-
+hp = 3
 PLAYER_PATH = 'images\player_iluha.png'
 OBSTACLE_PATH = 'images\woman.png'
 
@@ -29,38 +29,75 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.image.load(PLAYER_PATH)
-        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = 0
 
-    # def update(self):
-    #     print('prizhok')
+    def update(self):
+        # print(self.rect.y)
+        if self.rect.y > 300:
+            self.rect.y = 300
+            self.speed = 0
+        elif self.rect.y == 300:
+            self.rect.y += self.speed
+        else:
+            self.rect.y += self.speed
+            self.speed += 1
+        
+            
+    def jump(self):
+        if self.rect.y >= 295:
+            # self.rect.y += self.speed
+            # print(1)
+            self.speed = -20
+            self.update()
+            # self.rect.y -= 100
+        
+    def drop(self):
+        self.rect.y = 300
+        self.speed = 0
 
 obstacles_sprite = pygame.sprite.Group()
 player_sprite = pygame.sprite.Group()
-player_sprite.add(Player(130, HEIGHT - 50))
+j = Player(130, HEIGHT - 200)
+player_sprite.add(j)
+
 
 time_spawn = 0
 
 run = 1
 
-print(111)
+# print(111)
+random_time = random.randint(10, 20) * 100
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                j.jump()
+            if event.key == pygame.K_LSHIFT:
+                j.drop()
         
     time_now = pygame.time.get_ticks()
-    random_time = random.randint(1000, 3000)
+    # print(random_time)
 
     if time_now - time_spawn >= random_time:
         obstacles_sprite.add(Obstacle(WIDTH, HEIGHT - 100))
         time_spawn = time_now
+        random_time = random.randint(5, random_time // 100 + 5) * 100
     
+    j.update()
     obstacles_sprite.update()
+    if pygame.sprite.spritecollide(j, obstacles_sprite, True):
+        print('ОЙ ЙОЙ ЙОЙ')
+        hp -= 1
+        print('HP', hp)
 
     screen.fill((255, 255, 255))
     obstacles_sprite.draw(screen)
+    player_sprite.draw(screen)
     pygame.display.update()
 
     clock.tick(60)
